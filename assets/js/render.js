b70fc3459db0c5a6295b3d51f9bd90e00fd05b0a
Javier Morales <javier.morales.arenas94@gmail.com>
2026-09-18 18:59:56 -0600

    Feature: renderizado de modo oscuro

---
+++ assets/js/render.js
@@ -0,0 +1,22 @@
+//JS enfocado en renderizar elementos generales en todos los HTML
+
+//Render para navbar
+export function renderizarNavbar (ruta, contenedor)
+{
+    contenedor.innerHTML= "";
+
+    //Verificamos la ruta o ruta porfecto
+    if (xx) {
+        
+    }
+
+    contenedor.innerHTML= ``; //codigo navbar
+
+    //apicar el modo oscuro
+    // if (condition) {
+    //     contenedor.innerHTML= renderizarNavbar(./, main,true)`HTML del modo oscuro`;
+    // }
+    // else{
+    //     contenedor.innerHTML= `html del modo claro`;
+    // }
+}
